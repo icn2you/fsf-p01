28569f9da8569b15894d11bb53fe88c03459ceda
Yuko Uda <yuko.uda0110@gmail.com>
2020-04-03 15:57:42 -0700

    Create queryURL for ajax call for recipes page

--- src/javascript/recipes-page.js
+++ src/javascript/recipes-page.js
@@ -11,13 +11,34 @@ import _ from 'lodash';
 import 'materialize-css';
 
 const queryString = require('query-string');
+const separator = ',';
 
 const renderRecipesPage = () => {
   console.log(location.search);
   const parsed = queryString.parse(location.search);
   console.log(parsed);
-  const searchObj = JSON.parse(atob(parsed['params']));
+  const searchObj = JSON.parse(atob(parsed.params));
   console.log(searchObj);
+
+  // searchObj = {
+  //   diet: diet,
+  //   intolerances: intolerances,
+  //   ingredients: searchIngredients
+  // };
+
+  const recipeURL = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/';
+  const diet = searchObj.diet;
+  console.log('diet: ' + diet);
+  const intolerances = _.join(searchObj.intolerances, separator);
+  console.log('intolerances: ' + intolerances);
+  const ingredients = _.join(searchObj.ingredients, separator);
+  console.log('ingredients: ' + ingredients);
+  const number = 100;
+// https://spoonacular.com/food-api/docs#Recipe-Sorting-Options
+  const sort = 'popularity';
+
+  const queryURL = `${recipeURL}complexSearch?diet=${diet}&intolerances=${intolerances}&includeIngredients=${ingredients}&instructionsRequired=true&addRecipeInformation=true&sort=${sort}&number=${number}&limitLicense=true`;
+  console.log(queryURL);
 };
 
 const recipesPage = () => {
