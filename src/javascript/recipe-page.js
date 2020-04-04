4f504c9a055cc4437a96a8b6d0b37440408069c2
Christopher Zenner <christopher.zenner@gmail.com>
2020-04-03 19:17:30 -0700

    feat: Add complete prototype of recipe detail page
    - [X] Add code to dynamically render recipe on recipe detail page
      (`recipe.html`).
    - Modify recipe detail page script to export itself (`recipe-page.js`).
    - Modify primary script to import recipe detail page (`index.js`).

--- src/javascript/recipe-page.js
+++ src/javascript/recipe-page.js
@@ -1,51 +1,60 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import 'materialize-css';
+
+import { SPOONACULAR_API_KEY } from '../../config/keys';
+
+const recipePage = () => {
 // ********** Variables for QueryURLs **********
-const recipeURL = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/';
-const recipeID = '663323';
+  const recipeURL = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/';
+  const recipeID = '663323';
 
-// ********** QueryURL **********
-// https://spoonacular.com/food-api/docs#Get-Recipe-Information
-// https://api.spoonacular.com/recipes/:id/information?apiKey=###&includeNutrition=false
-const queryURL = `${recipeURL}${recipeID}/information`;
+  // ********** QueryURL **********
+  // https://spoonacular.com/food-api/docs#Get-Recipe-Information
+  // https://api.spoonacular.com/recipes/:id/information?apiKey=###&includeNutrition=false
+  const queryURL = `${recipeURL}${recipeID}/information`;
 
-$.ajax({
-  url: queryURL,
-  method: 'GET',
-  headers: {
-    "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-    "x-rapidapi-key": SPOONACULAR_API_KEY
-  },
-}).then((response) => {
-  console.log('ajax1 | successful!!!!');
-  console.log(response);
+  $.ajax({
+    url: queryURL,
+    method: 'GET',
+    headers: {
+      "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+      "x-rapidapi-key": SPOONACULAR_API_KEY
+    },
+  }).then((response) => {
+    console.log('ajax1 | successful!!!!');
+    console.log(response);
 
-  const tr = $(`<tr id=${response.id}>`);
-  const idTd = $('<td>').text(response.id);
-  const titleTd = $('<td>').text(response.title);
-  const imgEl = $('<img>').attr('src', response.image);
-  const imgTd = $('<td class="image">').append(imgEl);
-  const minutesTd = $('<td>').text(response.readyInMinutes);
-  const summaryTd = $('<td>').html(response.summary);
-  const dietsTd = $('<td>');
-  let dietsText = '';
-  response.diets.forEach((diet) => {
-    dietsText += `${diet}, `;
+    // unused recipe elements
+    // _.forEach(response.diets, diet => {
+    //   dietOpts += `<option value="${_.toLower(val)}">${val}</option>`;
+    // });
+    
+    $('#recipe-title').text(response.title); // recipe title
+    $('#recipe-time').text(response.readyInMinutes); // recipe prep time
+    $('#recipe-img').attr('src', response.image); // recipe image
+    $('#no-servings > option[val="options"]').attr({ 'selected': false });
+    $(`#no-servings > option[val="${response.servings}"]`).attr({ 
+      'disabled' : true,
+      'selected' : true 
+    });
+
+    let ingredients = '';
+
+    _.forEach(response.extendedIngredients, ingredient => {
+      ingredients += `<li>${ingredient.amount} ${_.toLower(ingredient.unit)} 
+        ${ingredient.name}</li>`;
+    });
+
+    $('#recipe-ingredients').html(ingredients);
+    $('#recipe-summary').html(response.summary)
+
+    $('#recipe-instructions').html(response.instructions);
+
+    console.log(`recipeID: ${recipeID}`);
+  }).catch(function (error) {
+    console.log(`${error.status} ${_.toUpper(error.statusText)}`);
   });
-  dietsTd.text(dietsText);
-  const servingTd = $('<td>').text(response.servings);
-  const ingredientsTd = $('<td class="ingredients">').append($('<ul>'));
-  response.extendedIngredients.forEach((ingredient) => {
-    const ingItemEl = $('<li>');
-    const ingNameEl = $('<span>').text(ingredient.name);
-    const ingAmountEl = $('<span>').text(` - ${ingredient.amount}${ingredient.unit}`);
-    ingItemEl.append(ingNameEl, ingAmountEl);
-    ingredientsTd.append(ingItemEl);
-  });
+};
 
-  const instructionTd = $('<td class="instruction">').html(response.instructions);
-  tr.append(idTd, titleTd, imgTd, minutesTd, dietsTd, servingTd, ingredientsTd, summaryTd, instructionTd);
-  $('#recipe .result tbody').append(tr);
-
-  console.log('recipeIDs: ' + recipeIDs);
-}).catch(function (error) {
-  console.log(`${error.status} ${error.statusText.toUpperCase()}`);
-});
+export default recipePage;
